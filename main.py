020e64dc3109dfeb49d3934787e96f8e23ba2fc4
ChuckD95 <namnguyen1995@hotmail.com>
2025-10-01 03:31:02 +1000

    game limited to 60fps

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while 1 > 0:
-        screen.fill(pygame.Color('black'))
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        screen.fill(pygame.Color('black'))
         pygame.display.flip()
+        dt = clock.tick(60)/1000
         
 if __name__ == "__main__":
     main()
